0a3b87a66171891be294ae1e03abe4e2119f535b
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-02-18 23:39:57 -0700

    increment questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var timerEl = document.querySelector('#countdown');
-var startQuizEl = document.querySelector('#start-quiz')
-var listItemEl = document.querySelector('.answer')
+var startQuizEl = document.querySelector('#start-quiz');
+var listItemEl = document.querySelector('.answer');
 
-var preQuiz = document.querySelector('.pre-quiz')
-var questions = document.querySelector('.questions')
+var preQuiz = document.querySelector('.pre-quiz');
+var questions = document.getElementsByClassName('questions');
+var counter = 0;
 
 var timeLeft = 60;
 
@@ -30,28 +31,33 @@ document.getElementById("start-quiz").addEventListener("click", setTimer);
 //function that enables question to display and calls on the question to increment
 function displayQuestions() {
     preQuiz.style.display = "none";
-    questions.style.display = "block";
+    questions[counter].style.display = "block";
 
-    incrementQuestions()
 }
 
 //function to allow questions to increment
 function incrementQuestions() {
+    questions[counter].style.display = "none";
+    counter++
+    displayQuestions()
 
 };
 
 //listens for a click to check for answer accuracy and displays whether the answer is right or wrong and decrements time by 10 if wrong
-listItemEl.addEventListener("click", function(event) {
-    var answer = event.target;
-
+document.querySelectorAll('.answer').forEach(item => {
+    item.addEventListener("click", event => {
+    //handle click
+        var answer = event.target;
+    console.log(answer)
     if (answer.dataset.answer = "Correct!") {
         answer.innerHTML = answer.dataset.answer
-    } else (answer.dataset.answer = "Wrong!") {
+    } else if (answer.dataset.answer = "Wrong!") {
         answer.innerHTML = answer.dataset.answer
+        timeLeft = timeLeft - 10;
     }
-
-    }
-});
+    incrementQuestions()
+    });
+  })
 
 //if time left === 0, then end quiz
 function endQuiz() {
